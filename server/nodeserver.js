4bf304d60219e7a49630101284ba704671796e33
Niklas Jurij Plessing <contact@niklasplessing.net>
2022-03-03 15:31:22 +0100

    fix server directory

--- server/nodeserver.js
+++ server/nodeserver.js
@@ -8,7 +8,7 @@ const app = express();
 // Setup view engine
 app.set('view engine', 'pug');
 
-app.use(express.static(path.resolve(path.join(__dirname, '/dist'))));
+app.use(express.static(path.resolve(path.join(__dirname, '/build'))));
 
 app.get('/', (req, res) => {
   res.render('index');
