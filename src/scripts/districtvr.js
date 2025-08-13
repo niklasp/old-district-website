989052f8eae761daa800d83ec1a8e71f9a779666
Niklas P <contact@niklasplessing.net>
2025-08-13 19:51:50 +0200

    node 22

--- src/scripts/districtvr.js
+++ src/scripts/districtvr.js
@@ -1,85 +1,86 @@
 import { ScrollSpy, Modal } from "bootstrap";
-import Swiper, { Navigation, Pagination } from 'swiper';
-import CC from 'CookieConsent';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/lazy';
-import 'cookieconsent/build/cookieconsent.min.css';
+import Swiper, { Navigation, Pagination } from "swiper";
+import CC from "cookieconsent";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/lazy";
+import "cookieconsent/build/cookieconsent.min.css";
 
-import '../images/club-entrance.jpg';
-
+import "../images/club-entrance.jpg";
 
 export default function DistrictVR() {
-  let postContainer = document.querySelector( '.medium-posts' );
+  let postContainer = document.querySelector(".medium-posts");
   let mediumArticles;
 
-  const updateMediumArticles = ( articles ) => {
-    const domArticles = [ ...postContainer.querySelectorAll( '.post' ) ];
-    articles.items.splice( 2 );
+  const updateMediumArticles = (articles) => {
+    const domArticles = [...postContainer.querySelectorAll(".post")];
+    articles.items.splice(2);
 
-    [ ...articles.items ].forEach( ( a, idx) => {
-      updateArticle( domArticles[ idx ], a );
+    [...articles.items].forEach((a, idx) => {
+      updateArticle(domArticles[idx], a);
     });
   };
 
-  const navButton = document.querySelector('button.hamburger');
-  const mobileMenu = document.querySelector('.mobile-menu');
-  const navLinks = mobileMenu?.querySelectorAll( 'a' );
+  const navButton = document.querySelector("button.hamburger");
+  const mobileMenu = document.querySelector(".mobile-menu");
+  const navLinks = mobileMenu?.querySelectorAll("a");
 
-  if ( navLinks ) {
-    [ ...navLinks ].forEach( link => {
-      link.addEventListener( 'click', () => {
-        mobileMenu.classList.remove( 'open' );
-        navButton.classList.remove( 'is-active' );
+  if (navLinks) {
+    [...navLinks].forEach((link) => {
+      link.addEventListener("click", () => {
+        mobileMenu.classList.remove("open");
+        navButton.classList.remove("is-active");
       });
     });
   }
 
-  navButton?.addEventListener( 'click', () => {
-    navButton.classList.toggle( 'is-active' );
-    mobileMenu.classList.toggle( 'open' );
+  navButton?.addEventListener("click", () => {
+    navButton.classList.toggle("is-active");
+    mobileMenu.classList.toggle("open");
   });
 
-  const updateArticle = ( domEl, article ) => {
-    if ( ! domEl ) {
+  const updateArticle = (domEl, article) => {
+    if (!domEl) {
       return;
     }
 
-    const dateOptions = {year: 'numeric', month: 'long', day: 'numeric' };
+    const dateOptions = { year: "numeric", month: "long", day: "numeric" };
 
-    const image = domEl.querySelector('img');
-    const title = domEl.querySelector('h4');
-    const desc = domEl.querySelector('.description');
-    const more = domEl.querySelector('.more a');
-    const cats = domEl.querySelector('.categories');
-    const date = domEl.querySelector( '.post-date' );
+    const image = domEl.querySelector("img");
+    const title = domEl.querySelector("h4");
+    const desc = domEl.querySelector(".description");
+    const more = domEl.querySelector(".more a");
+    const cats = domEl.querySelector(".categories");
+    const date = domEl.querySelector(".post-date");
 
     image.src = article.thumbnail;
     title.innerHTML = article.title;
     more.href = article.link;
     desc.innerHTML = article.description;
-    const newP = desc.querySelector( 'p:first-of-type' );
+    const newP = desc.querySelector("p:first-of-type");
     desc.innerHTML = newP.innerHTML;
-    cats.innerHTML = article.categories.join( ', ' );
-    date.innerHTML = article.pubDate ? new Date( article.pubDate ).toLocaleDateString('en-US', dateOptions) : '';
+    cats.innerHTML = article.categories.join(", ");
+    date.innerHTML = article.pubDate
+      ? new Date(article.pubDate).toLocaleDateString("en-US", dateOptions)
+      : "";
 
-//     $re = '/(<p>.*?<\/p>)/m';
-// preg_match_all($re, $str, $matches, PREG_SET_ORDER, 0);
+    //     $re = '/(<p>.*?<\/p>)/m';
+    // preg_match_all($re, $str, $matches, PREG_SET_ORDER, 0);
   };
 
   const initSwiper = () => {
-    const swiper = new Swiper('.swiper', {
-      modules: [ Navigation, Pagination ],
+    const swiper = new Swiper(".swiper", {
+      modules: [Navigation, Pagination],
       loop: true,
       preloadImages: false,
       lazy: true,
       pagination: {
-        el: '.swiper-pagination',
+        el: ".swiper-pagination",
         clickable: true,
       },
       on: {
-        click: function ( e ) {
+        click: function (e) {
           e.slideNext();
         },
       },
@@ -87,41 +88,41 @@ export default function DistrictVR() {
   };
 
   const init = () => {
-    if ( postContainer ) {
-    fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/thedistrictvr')
-      .then( resp => resp.json() )
-      .then( articles => updateMediumArticles( articles ) )
-      .catch( () => {
-        if ( postContainer.querySelector( '.post-wrap' ) ) {
-          postContainer.querySelector( '.post-wrap' ).innerHTML =
-            '<div class="col text-center" style="color: var(--color-secondary)"><p>Error getting posts from Medium, please try again later or navigate to medium directly with the link below.<p></div>';
-        }
-      });
+    if (postContainer) {
+      fetch(
+        "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/thedistrictvr"
+      )
+        .then((resp) => resp.json())
+        .then((articles) => updateMediumArticles(articles))
+        .catch(() => {
+          if (postContainer.querySelector(".post-wrap")) {
+            postContainer.querySelector(".post-wrap").innerHTML =
+              '<div class="col text-center" style="color: var(--color-secondary)"><p>Error getting posts from Medium, please try again later or navigate to medium directly with the link below.<p></div>';
+          }
+        });
     }
 
     new ScrollSpy(document.body, {
-      target: '#main-nav',
+      target: "#main-nav",
     });
 
     initSwiper();
-    
+
     window.cookieconsent.initialise({
-      "palette": {
-        "popup": {
-          "background": "#000a",
-          "text": "#0f0"
+      palette: {
+        popup: {
+          background: "#000a",
+          text: "#0f0",
         },
-        "button": {
-          "background": "transparent",
-          "text": "#0f0",
-          "border": "#0f0"
-        }
+        button: {
+          background: "transparent",
+          text: "#0f0",
+          border: "#0f0",
+        },
       },
-      "position": "bottom-right"
+      position: "bottom-right",
     });
   };
-
-
 
   init();
 }
